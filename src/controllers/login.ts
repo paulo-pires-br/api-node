e2b4fd7321dc0dbc11ce529c38ab9f33f998d3ea
Paulo Pires <ppiresmat@gmail.com>
2025-09-22 10:08:22 -0300

    feita conexao com o BD

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,6 +1,15 @@
 import express, {type Request, type Response} from 'express';
+import { AppDataSource } from '../data-source.js';
 
 const router = express.Router();
+
+AppDataSource.initialize()
+.then(()=>{
+    console.log("Conexão com o BD realizada com sucesso");
+})
+.catch((error)=>{
+    console.log("Erro na conexão com o BD:",error);
+})
 
 router.get("/",(req:Request, res:Response)=>{
     res.send("Bem-vindo Celke!")
