34cef95a168397a552239527fb37819222e35cd6
Paulo Pires <ppiresmat@gmail.com>
2025-09-22 10:54:48 -0300

    usado o host do arquivo env

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
     username: "root",
     password: "123456A#",
