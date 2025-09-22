347ec2d4e634e858f59d78b3c892951d748ca4a5
Paulo Pires <ppiresmat@gmail.com>
2025-09-22 11:00:08 -0300

    ajustado o campo DB_USERNAME para usar o arquivo env

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST,
     port: 3306,
-    username: "root",
+    username: process.env.DB_USERNAME,
     password: "123456A#",
     database: "celke",
     synchronize: false,
