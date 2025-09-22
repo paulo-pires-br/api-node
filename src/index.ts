094cf2311e160cea464ea09f99b1736c5a73ba9b
Paulo Pires <ppiresmat@gmail.com>
2025-09-22 09:10:50 -0300

    criada rota get

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import express, {type Request, type Response} from 'express';
 
 const app = express();
 
+app.get("/",(req:Request, res:Response)=>{
+    res.send("Bem-vindo Celke!")
+});
+
 app.listen(8080,()=>{
     console.log("Servidor iniciado na porta 8080: http://localhost:8080");
 });
